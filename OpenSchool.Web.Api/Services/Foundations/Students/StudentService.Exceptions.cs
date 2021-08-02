66d22381f41c24326ef642ecd51dfa38c1832c7c
ahmednreldin <m-32500912@s-mu.edu.eg>
2021-08-02 19:37:16 +0200

    Code clean up

--- OpenSchool.Web.Api/Services/Foundations/Students/StudentService.Exceptions.cs
+++ OpenSchool.Web.Api/Services/Foundations/Students/StudentService.Exceptions.cs
@@ -25,7 +25,7 @@ namespace OpenSchool.Web.Api.Services.Foundations.Students
             private StudentValidationException CreateAndLogValidationException(Exception exception)
             {
                 var studentValidationException = new StudentValidationException(exception);
-                this.loggerBroker.LogError(studentValidationException);
+                this.loggerBroker.LogError(studentValidationException.Message);
 
                 return studentValidationException;
             }
