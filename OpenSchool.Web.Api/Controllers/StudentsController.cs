cd06a4e2ea950b53ce11db28696361a98571286c
ahmednreldin <m-32500912@s-mu.edu.eg>
2021-08-03 21:00:42 +0200

    Controller : Post Student

--- OpenSchool.Web.Api/Controllers/StudentsController.cs
+++ OpenSchool.Web.Api/Controllers/StudentsController.cs
@@ -1,32 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OpenSchool.Web.Api.Brokers.Storages;
 using OpenSchool.Web.Api.Models.Students;
 using OpenSchool.Web.Api.Models.Students.Exceptions;
 using OpenSchool.Web.Api.Services.Foundations.Students;
+using RESTFulSense.Controllers;
+using System;
+using System.Threading.Tasks;
 
 namespace OpenSchool.Web.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StudentsController : ControllerBase
+    public class StudentsController : RESTFulController
     {
         private readonly IStudentService studentService;
-
-        public StudentsController(IStudentService studentService)
+        private readonly IStorageBroker storageBroker;
+        public StudentsController(IStorageBroker storageBroker , IStudentService studentService)
         {
+            this.storageBroker = storageBroker;   
             this.studentService = studentService;
         }
         [HttpPost]
-        public IActionResult PostStudent(Student student)
+        public async ValueTask<ActionResult<Student>> PostStudentAsync(Student student)
         {
             try
             {
-                studentService.RegisterStudentAsync(student);
+                Student registerStudent = 
+                    await this.studentService.RegisterStudentAsync(student);
+                return Created(registerStudent);
             }
-            catch (InvalidStudentException ex)
+            catch (StudentValidationException studentValidationException) 
+                when(studentValidationException.InnerException is AlreadyExistsStudentException)
             {
-                return BadRequest(ex.Message);
+                string innerMessage = GetInnerMessage(studentValidationException);
+                return Conflict(innerMessage);
             }
-            return Ok("Student Posted");
+            catch (StudentValidationException studentValidationException)
+            {
+                string innerMessage = GetInnerMessage(studentValidationException);
+                return BadRequest(innerMessage);
+            }
         }
+
+        private static string GetInnerMessage(Exception exception) =>
+            exception.InnerException.Message;
+        
     }
 }
